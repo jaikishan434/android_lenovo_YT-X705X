c6dceb94df773328257cefa7be2f70ce4218357b
Alhadi1024 <117118314+Alhadi1024@users.noreply.github.com>
2023-08-23 02:44:46 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_YT-X705X.mk
+    $(LOCAL_DIR)/omni_YTX705X.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_YT-X705X-user \
-    omni_YT-X705X-userdebug \
-    omni_YT-X705X-eng
+    omni_YTX705X-user \
+    omni_YTX705X-userdebug \
+    omni_YTX705X-eng
